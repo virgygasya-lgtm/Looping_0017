f2f9a468ce3c1c1159c080aa2408f87820086bd6
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-03-30 15:55:21 +0700

    looping.cpp

---
+++ looping.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main(){
+    int x;
+
+    //Perulangan dengan FOR
+    //mencetak tulisan sebanyak 5 kali
+    cout<<"PERULANGAN FOR"<<endl;
+    for(int i=0; i<=4; i++){
+        cout<<"Teknologi Informasi UMY"<<endl;
+    }
+    cout<<endl;
+
+    //untuk mendapatkan urutan angka acak yg berbeda
+    srand(time(0));
+
+    //Perulangan dengan WHILE
+    cout<<"PERULANGANN WHILE"<<endl;
+    x = 1 + rand()%10;
+
+    while(x<=5){
+        cout<<"Bilangan acak = "<<x <<endl;
+        x = rand()%10;
+    }
+
+    cout<<"Bilangan acak while yang terakhir ="<<x <<endl;
+    cout<<endl;
+
+    //Perulangan dengan DO...WHILE
+    cout<<"PERULANGANN DO...WHILE"<<endl;
+    x = 1 + rand()%10;
+
+    do{
+        cout<<"Bilangan acak = "<<x <<endl;
+        x = rand()%10;
+    }while(x<=5);
+
+    cout<<"Bilangan acak do-while yang terakhir ="<<x <<endl;
+    cout<<endl;
+
+    return 0;
+}
